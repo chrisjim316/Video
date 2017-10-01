7e994edae26ad5c017336cc35bea6422844ddbdb
Chris Jim <chrisjim316@gmail.com>
2017-10-01 12:24:20 +0100

    merge sendMessage() funcrtions

--- js/chat.js
+++ js/chat.js
@@ -31,6 +31,10 @@ function sendMessage() {
   
   skylink.sendP2PMessage(input.value);
   input.value = '';
+  /*
+  var text = $("#text").val();
+  $("#chatOutput").append(text + "<br>");
+  */
 }
 
 function addMessage(message, className) {
@@ -65,7 +69,10 @@ skylink.on('incomingMessage', function(message, peerId, peerInfo, isSelf) {
     user = peerInfo.userData.name || peerId;
     className = 'message';
   }
-  addMessage(user + ': ' + message.content, className);
+  var content = message.content;
+  addMessage(user + ': ' + content, className);
+  getRelevantInfo(content);
+  getSuggestions(content);
 });
 
 function getParameterByName(name, url) {
@@ -76,4 +83,55 @@ function getParameterByName(name, url) {
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getRelevantInfo (text) {
+  $.ajax({
+    url: wikiAPI,
+    data: { action: 'query', list: 'search', srsearch: text, srlimit: 1, format: 'json' }, //srsearch searches for the first result
+    dataType: 'jsonp',
+    /* generate html for the first search result */
+    success: function (data) {
+      var html = '';  
+      data.query.search.map(function(res) {
+        html += '<div class="well">';
+        html += res.snippet;
+        html += '</div>';
+      });  
+      $("#wiki").html(html);
+    }
+  });//end ajax function
+
+}
+
+function getSuggestions (text) {
+  $.ajax({
+    url: wikiAPI,
+    data: { action: 'query', list: 'search', srsearch: text, format: 'json' }, //srsearch searches for all results associated with the keywords     
+    dataType: 'jsonp',
+    /* generate html for each search result (10 results by default) */
+    success: function (data) {
+      var html = '';
+          html += '  <div class="row row-centered">';
+      
+      data.query.search.map(function(res) {
+        html += '    <div class="col-md-6">';
+        html += '      <a href="https://en.wikipedia.org/wiki/' + res.title + '" target="_blank">';
+        html += '        <div class="panel panel-default" style="position:relative;">';
+        html += '          <div class="panel-heading">';
+        html += '            <h3 class="panel-title"><strong>' + res.title + '</strong></h3>';
+        html += '          </div>';
+        html += '          <div class="panel-body">';
+        html += '            ' + res.snippet;
+        html += '          </div>';
+        html += '        </div>';
+        html += '      </a>';
+        html += '    </div>';
+      });
+      
+      html += '  </div>';
+      
+      $("#suggestions").html(html);
+    }
+    });//end ajax function
 }
